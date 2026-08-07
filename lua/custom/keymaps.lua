20a7584c4ca46c4833fd60246e66cf3514b78a7e
Gab <gbrlferz@gmail.com>
2026-08-07 03:02:55 -0300

    Change keymap F5 shortcut exe path

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -12,15 +12,14 @@ end, { desc = 'Run [B]uild script' })
 
 vim.keymap.set('n', '<F5>', function()
   local is_windows = vim.fn.has 'win32' == 1
-  local build_dir = 'build'
-
-  local exe_path = nil
+  local run_dir = 'build/win/debug'
+  local exe_path = ''
 
   if is_windows then
-    local files = vim.fn.glob(build_dir .. '/*.exe', false, true)
+    local files = vim.fn.glob(run_dir .. '/*.exe', false, true)
     if #files > 0 then exe_path = files[1] end
   else
-    local files = vim.fn.glob(build_dir .. '/*', false, true)
+    local files = vim.fn.glob(run_dir .. '/*', false, true)
     for _, file in ipairs(files) do
       if vim.fn.executable(file) == 1 and vim.fn.isdirectory(file) == 0 then
         exe_path = file
@@ -31,12 +30,7 @@ vim.keymap.set('n', '<F5>', function()
 
   if exe_path then
     vim.notify('Starting ' .. exe_path .. '...', vim.log.levels.INFO)
-
-    if is_windows then
-      vim.fn.jobstart('.\\' .. exe_path:gsub('/', '\\'))
-    else
-      vim.fn.jobstart('./' .. exe_path)
-    end
+    vim.fn.jobstart { exe_path }
   else
     vim.notify('No executable or run script found.', vim.log.levels.WARN)
   end
